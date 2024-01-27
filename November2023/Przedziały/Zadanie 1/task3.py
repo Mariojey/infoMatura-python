982127d2be508f4dfe35471b05157246da5b1571
Mariusz Jacek <marios.yaceg@gmail.com>
2024-01-27 19:09:21 +0100

    Zestaw 2 Zadanie 2.3

--- November2023/Przedziały/Zadanie 1/task3.py
+++ November2023/Przedziały/Zadanie 1/task3.py
@@ -1,5 +1,8 @@
 file = open("przedzialy2.txt", "r")
 text = file.realines()
+first_line = text.strip().split(',')
+from_value = [first_line[0][1:]]
+to_value = [first_line[1][:-1]]
 for line in text:
     parsedLine = line.strip()
     divs = parsedLine.split(',')
